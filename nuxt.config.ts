3a048097a2fd72624b63ab57ee1bfec350adaaf8
Yui Saito <ysaito@t-sol.biz>
2024-02-09 14:46:25 +0900

    metaタグなど追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,4 +10,10 @@ export default defineNuxtConfig({
     serviceDomain: SERVICE_DOMAIN
   },
   css: ['~/assets/css/reset.css','~/assets/css/style.css',],
+  app:{
+    head:{
+      charset:'utf-8',
+      viewport:'width=device-width, initial-scale=1',
+    }
+  }
 })
--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+import { config } from './settings/siteSettings'
+</script>
 <template>
+<div>
+  <Head>
+    <Title>{{ config.siteName }}</Title>
+     <Meta name="主に日記です" :content="title" />
+     <Style type="text/css" ></Style>
+  </Head>
   <Header />
   <NuxtPage />
   <Footer />
+</div>
 </template>
